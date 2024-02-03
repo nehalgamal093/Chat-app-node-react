f566fc7d8b4cd5f8a47682e472fd1293589582c3
nehalgamal093 <nehalgamal093@gmail.com>
2024-02-03 18:49:46 +0200

    picking avatar

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -6,7 +6,6 @@ import { useState, useEffect } from 'react';
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import axios from 'axios';
-import { registerRoute } from '../utils/APIRouts';
 
 
 function Register() {
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,12 @@
-const { register, login } = require("../controllers/userController.js");
+const {
+  register,
+  login,
+  setAvatar,
+} = require("../controllers/userController.js");
 
 const router = require("express").Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.post("/setAvatar/:id", setAvatar);
 module.exports = router;
